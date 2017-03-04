c44af8b523ae57a88234bbf2738e4e3826ece0ca
Michelle Brideau <pair+yhzgirl@gmail.com>
2017-03-04 09:58:50 +0000

    chore(setup): initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a redo of the heistly made orginal Cycling Across Canada website
+
+Cycling across Canada site (using github pages)
+
+The site used to document cycling across Canada the summer of 2016.
+
+
---
+++ app/main.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+// export default () => <h1>Hello World</h1>;
+
+/*
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+ */
