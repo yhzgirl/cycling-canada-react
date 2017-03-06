28579f18ec36daacee4edb1978c4bf5689b5acc3
Michelle Brideau <pair+yhzgirl@gmail.com>
2017-03-06 21:07:13 +0000

    fix(pure): fixing function

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,15 +3,11 @@ import Header from './header.jsx';
 
 require('../styles/App.scss');
 
-export default React.Class({
-  render() {
-    return (
-      <div>
-        <Header />
-        <div>
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
-});
+export default() =>
+  <div>
+    <Header />
+    <div>
+      {this.props.children}
+    </div>
+  </div>
+;
