de976b0d58a7b9f47087d2406825e674791dbf0b
Michelle Brideau <pair+yhzgirl@gmail.com>
2017-03-06 20:56:58 +0000

    feat(router): adding bootsrap react router

--- app/main.jsx
+++ app/main.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+import Home from './components/home.jsx';
+import About from './components/about.jsx';
 import App from './components/App';
 
-ReactDOM.render(
-  <App />,
-  document.body.appendChild(document.createElement('div'))
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+    </Route>
+  </Router>
+), document.getElementById('cycling-canada'));
+
