21d7ec486e12afb14ca8fa055d541801d8e8578d
Michelle Brideau <pair+yhzgirl@gmail.com>
2017-03-08 10:21:12 +0000

    fix(bug): work around for external link bug in react-bootstrap issue 2365

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav bsStyle="pills" className="menu" pullLeft>
-              <NavItem eventKey={1} target="_blank" href="https://www.youtube.com/channel/UCgZu2jNyVKDtqoq0zTUCDnw" >Vlog</NavItem>
+            <li role="presentation"><a href="https://www.youtube.com/channel/UCgZu2jNyVKDtqoq0zTUCDnw">Vlog</a></li>
 
             <LinkContainer to="/route_map">
               <NavItem eventKey={2} >Route</NavItem>
@@ -42,26 +42,30 @@ export default function Header() {
             </NavDropdown>
           </Nav>
           <Nav pullRight className="social-media">
-            <NavItem eventKey={5} href="#">
-              <a href="https://twitter.com/cycling_woman" target="_blank" rel="noopener noreferrer">
+            <li role="presentation">
+              <a href="https://twitter.com/cycling_woman">
                 <i className="fa fa-twitter fa-2x" />
               </a>
-            </NavItem>
-            <NavItem eventKey={6} href="#">
-              <a href="https://instagram.com/cycling_woman/" target="_blank" rel="noopener noreferrer">
+            </li>
+
+            <li role="presentation">
+              <a href="https://instagram.com/cycling_woman/">
                 <i className="fa fa-instagram fa-2x" />
               </a>
-            </NavItem>
-            <NavItem eventKey={7} href="#">
-              <a href="https://www.youtube.com/channel/UCgZu2jNyVKDtqoq0zTUCDnw?sub_confirmation=1&feature=iv&src_vid=mWC514efG40&annotation_id=annotation_2210725983" target="_blank" rel="noopener noreferrer">
+            </li>
+
+            <li role="presentation">
+              <a href="https://www.youtube.com/channel/UCgZu2jNyVKDtqoq0zTUCDnw">
                 <i className="fa fa-youtube-square fa-2x" />
               </a>
-            </NavItem>
-            <NavItem eventKey={8} href="#">
-              <a href="http://facebook.com/cyclingacrosscanada" target="_blank" rel="noopener noreferrer">
+            </li>
+
+            <li role="presentation">
+              <a href="http://facebook.com/cyclingacrosscanada">
                 <i className="fa fa-facebook fa-2x" />
               </a>
-            </NavItem>
+            </li>
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
